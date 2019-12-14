944e845715aef5866f2406516ecf749e0f8344e4
Sectimus <Sectimus@hotmail.co.uk>
2019-12-14 11:39:33 +0000

    added loading animations

--- public/js/index.js
+++ public/js/index.js
@@ -16,81 +16,118 @@ function loadSessions() {
 }
 
 function loadTalks() {
-  //display the session data
-  $.when(Talk.loadAll(), $.ready).done(function(data_talks) {
-    //setup talks global
-    talks = data_talks;
-    var template = $("#talks_template").html();
-    $.each(talks, function() {
-      //check average ratings
-      let average = (function getAverage(set) {
-        let total = 0;
-        for (let i = 0; i < set.length; i++) {
-          total += parseInt(set[i]);
-        }
-        var avg = total / set.length;
-        if (!total) {
-          return 0;
-        } else {
-          return avg;
-        }
-      })(this.ratings);
-      var filledStars = Math.floor(average * 1) / 1;
-      var partialStar = parseFloat((average % 1).toFixed(2)) * 100;
-
-      var html = $(Mustache.to_html(template, this));
-      //set each star width
-      (function fillStars() {
-        let filledPartial = false;
-        html.find(".star-over").each(function() {
-          let starNum = $(this).attr("star");
-          if (filledStars >= starNum) {
-            $(this).width("100%");
-          } else if (!filledPartial) {
-            $(this).width(partialStar + "%");
-            filledPartial = true;
+  //create holding div to append to (only children are returned)
+  var content = $("<div>");
+  //return a promise that has the html generated for this page
+  //this is executed when both the document is ready and talks have been loaded from the server
+  return $.when(Talk.loadAll(), $.ready)
+    .done(function(data_talks) {
+      //setup talks global
+      talks = data_talks;
+      var template = $("#talks_template").html();
+      $.each(talks, function() {
+        //check average ratings
+        let average = (function getAverage(set) {
+          let total = 0;
+          for (let i = 0; i < set.length; i++) {
+            total += parseInt(set[i]);
           }
-        });
-      })();
-      //set each star click handler to rate
-      (function setupRatingHandlers() {
-        html
-          .find(".ratings")
-          .children()
-          .each(function() {
-            //click function
-            $(this).on("click", function() {
-              var star = $(this);
-              var rating = star.attr("star");
-              //get the talk by using the mustache filled talk id to index into the talks global
-              var talk = talks[star.closest("[talkid]").attr("talkid") - 1];
+          var avg = total / set.length;
+          if (!total) {
+            return 0;
+          } else {
+            return avg;
+          }
+        })(this.ratings);
+        var filledStars = Math.floor(average * 1) / 1;
+        var partialStar = parseFloat((average % 1).toFixed(2)) * 100;
 
-              talk.rate(rating);
-            });
+        var html = $(Mustache.to_html(template, this));
+        //set each star width
+        (function fillStars() {
+          let filledPartial = false;
+          html.find(".star-over").each(function() {
+            let starNum = $(this).attr("star");
+            if (filledStars >= starNum) {
+              $(this).width("100%");
+            } else if (!filledPartial) {
+              $(this).width(partialStar + "%");
+              filledPartial = true;
+            }
           });
-      })();
+        })();
+        //set each star click handler to rate
+        (function setupRatingHandlers() {
+          html
+            .find(".ratings")
+            .children()
+            .each(function() {
+              //click function
+              $(this).on("click", function() {
+                var star = $(this);
+                var rating = star.attr("star");
+                //get the talk by using the mustache filled talk id to index into the talks global
+                var talk = talks[star.closest("[talkid]").attr("talkid") - 1];
 
-      $("#contentWrapper").append(html);
+                talk.rate(rating);
+              });
+            });
+        })();
+        content.append(html);
+      });
+    })
+    .then(function(asd) {
+      return content.children();
     });
-  });
 }
 
 $(document).ready(function() {
-  var t = new Talk(
-    1,
-    "Martin Fowler",
-    "Patterns of Enterprise Application Architecture",
-    "The practice of enterprise application development has benefited from the emergence of many new enabling technologies. Multi-tiered object-oriented platforms, such as Java and .NET, have become commonplace. These new tools and technologies are capable of building powerful applications, but they are not easily implemented. Common failures in enterprise applications often occur because their developers do not understand the architectural lessons that experienced object developers have learned.",
-    "A",
-    "9:00-10:00",
-    ["patterns, archtecture"],
-    []
-  );
-  /*
-    var session = Session.load();
-    session.addTalk(t);
-    */
+  $("#test").on("click", function() {
+    startSwitch("talks");
+    switchPage("talks", loadTalks());
+  });
 });
 
 //loadSessions();
-loadTalks();
+//loadTalks();
+//startSwitch("Session search");
+
+//takes a promise that child elements will be provided
+function switchPage(pagename, promise) {
+  var animation = startSwitch(pagename);
+  $.when(promise, animation, $.ready).done(function(content) {
+    endSwitch(content);
+  });
+}
+//show the loading pane
+function animationTransition(content) {
+  var animation = $("#contentWrapper")
+    .animate(
+      {
+        opacity: 0
+      },
+      150,
+      function() {
+        $("#contentWrapper").html(content);
+        $("#contentWrapper").animate(
+          {
+            opacity: 1
+          },
+          150
+        );
+      }
+    )
+    .promise();
+  return animation;
+}
+function startSwitch(pagename) {
+  var template = $("#loader_template").html();
+  var html = $(Mustache.to_html(template, { pagename: pagename }));
+  var animation = animationTransition(html);
+  return animation;
+}
+//switch to the new content
+function endSwitch(to) {
+  var animation = animationTransition(to);
+  return animation;
+}
